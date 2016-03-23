e2ec448d7c29b2a3d8f6f17423ebad7b2688684a
Gregory DeNinno <gpdno@mac.com>
2016-03-23 22:47:40 +0000

    added admin features to users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController <ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Blog post was sucessfull"
       redirect_to article_path(@article)
@@ -56,7 +56,7 @@ class ArticlesController <ApplicationController
     end
   
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user && !current_user.admin?
         flash[:danger] = "You can only edit or delete your own articles"
         redirect_to :back
       end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController <ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
   before_action :require_same_user, only: [:edit, :update]
+  before_action :require_admin, only: [:destroy]
   
   def new
     @user = User.new
@@ -9,6 +10,7 @@ class UsersController <ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome, #{@user.username} to my blog"
       redirect_to articles_path
     else
@@ -38,6 +40,13 @@ class UsersController <ApplicationController
     @users = User.all
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all associated articles are deleted"
+    redirect_to users_path
+  end
+  
   private
     
     def set_user
@@ -49,8 +58,15 @@ class UsersController <ApplicationController
     end
     
     def require_same_user
-      if current_user != @user
+      if current_user != @user && !current_user.admin?
         flash[:danger] = "You can only edit your own account"
+        redirect_to root_path
+      end
+    end
+    
+    def require_admin
+      if logged_in? && !current_user.admin?
+        flash[:danger] = "access denied"
         redirect_to root_path
       end
     end
